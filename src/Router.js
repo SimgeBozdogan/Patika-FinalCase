c07d0a9072059759cc557f1d7345b3d132eda224
SimgeBozdogan <80451730+Simge-157370@users.noreply.github.com>
2024-01-02 11:32:05 +0300

    * Handle redirecting to home page when user is in localhost:3000 path.

--- src/Router.js
+++ src/Router.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CreateApplication from './pages/Application/CreateApplication';
-import ApplicationSuccess from './pages/Application/ApplicationSuccess';
-import QueryApplication from './pages/Application/QueryApplication';
-import ViewApplication from './pages/Application/ViewApplication';
-import AdminLogin from './pages/Admin/AdminLogin';
-import AdminApplicationList from './pages/Admin/AdminApplicationList';
-import AdminViewApplication from './pages/Admin/AdminViewApplication';
-import NotFound from './components/NotFound';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CreateApplication from "./pages/Application/CreateApplication";
+import ApplicationSuccess from "./pages/Application/ApplicationSuccess";
+import QueryApplication from "./pages/Application/QueryApplication";
+import ViewApplication from "./pages/Application/ViewApplication";
+import AdminLogin from "./pages/Admin/AdminLogin";
+import AdminApplicationList from "./pages/Admin/AdminApplicationList";
+import AdminViewApplication from "./pages/Admin/AdminViewApplication";
+import NotFound from "./components/NotFound";
+import { RedirectToHomePage } from "./pages/RedirectToHomePage";
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<RedirectToHomePage />} />
         <Route path="/basvuru-olustur" element={<CreateApplication />} />
         <Route path="/basvuru-basarili" element={<ApplicationSuccess />} />
         <Route path="/basvuru-sorgula" element={<QueryApplication />} />
         <Route path="/basvuru/:basvuruNo" element={<ViewApplication />} />
         <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/admin/basvuru-listesi" element={<AdminApplicationList />} />
-        <Route path="/admin/basvuru/:basvuruNo" element={<AdminViewApplication />} />
+        <Route
+          path="/admin/basvuru-listesi"
+          element={<AdminApplicationList />}
+        />
+        <Route
+          path="/admin/basvuru/:basvuruNo"
+          element={<AdminViewApplication />}
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
